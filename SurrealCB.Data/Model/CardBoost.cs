5c5d6d804977dfe28209eb7b3be92cc15f37b6fa
fercaveri <ferc4@hotmail.com>
2020-03-31 14:32:59 -0300

    Implementing stats

--- SurrealCB.Data/Model/CardBoost.cs
+++ SurrealCB.Data/Model/CardBoost.cs
@@ -2,13 +2,9 @@ using System;
 
 namespace SurrealCB.Data.Model
 {
-    public class CardBoost : IEntity
+    public class CardBoost : Stats
     {
-        public int Hp { get; set; }
-        public int Atk { get; set; }
-        public int Def { get; set; }
-        public int Imm { get; set; }
-        public double Spd { get; set; }
+        
         public CardPassive Passive { get; set; }
         //Si quiero mejorar una pasiva existente usar un nuevo cardpassive con el mismo id
 
