6958979a68d3d129fd2467026524464469c8ff09
fercaveri <ferc4@hotmail.com>
2020-03-31 14:43:30 -0300

    Create Object.cs

---
+++ SurrealCB.Data/Model/Object.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SurrealCB.Data.Model
+{
+    public class Object : IEntity
+    {
+        public string Name;
+        public int Value;
+        public ObjectType Type;
+        public int Tier;
+        
+        
+    }
+}
