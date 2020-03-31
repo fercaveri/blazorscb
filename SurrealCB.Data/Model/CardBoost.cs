966f528a038a4e07c8761041538b293c189d4b46
fercaveri <ferc4@hotmail.com>
2020-03-30 21:47:46 -0300

    More class changes

--- SurrealCB.Data/Model/CardBoost.cs
+++ SurrealCB.Data/Model/CardBoost.cs
@@ -7,11 +7,10 @@ namespace SurrealCB.Data.Model
         public int Hp { get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
+        public int Imm { get; set; }
         public double Spd { get; set; }
         public CardPassive Passive { get; set; }
-        public string Param1 { get; set; }
-        public string Param2 { get; set; }
-        public string Param3 { get; set; }
+        //Si quiero mejorar una pasiva existente usar un nuevo cardpassive con el mismo id
 
         //Hacer un level boost y hacer q nivel es y tener varios de un nivel para poder hacer arbol habilidades
 //Fijafme si este lahout sirve o si dividir en dos tipos de boost pa runa y level
@@ -21,5 +20,7 @@ Public class LevelBoost : IEntity {
 public int Level { get; set; }
 Public CardBoost Boost {get; set;}
 Public string ImprovedName {get; set;}
+//Public List<RequiredObject> RequiredObjects {get; set;}
+Public int Cost {get; set;}
 }
 }
