514009c1af0f3f3b40a5f9ad0a07937b0e55ef89
fercaveri <ferc4@hotmail.com>
2020-03-30 21:21:01 -0300

    Update CardBoost.cs

--- SurrealCB.Data/Model/CardBoost.cs
+++ SurrealCB.Data/Model/CardBoost.cs
@@ -12,5 +12,14 @@ namespace SurrealCB.Data.Model
         public string Param1 { get; set; }
         public string Param2 { get; set; }
         public string Param3 { get; set; }
+
+        //Hacer un level boost y hacer q nivel es y tener varios de un nivel para poder hacer arbol habilidades
+//Fijafme si este lahout sirve o si dividir en dos tipos de boost pa runa y level
     }
+
+Public class LevelBoost : IEntity {
+public int Level { get; set; }
+Public CardBoost Boost {get; set;}
+Public string ImprovedName {get; set;}
+}
 }
