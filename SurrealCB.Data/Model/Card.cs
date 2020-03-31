7fa21d454b4b8d37eadca131bd5122cdc0f46a4f
fercaveri <ferc4@hotmail.com>
2020-03-31 14:32:12 -0300

    Stats model

--- SurrealCB.Data/Model/Card.cs
+++ SurrealCB.Data/Model/Card.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 
 namespace SurrealCB.Data.Model
 {
-    public class Card : IEntity
+Public abstract class Stats : IEntity {
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Imm { get; set; } //Immunity
+        public double Spd { get; set; }
+}
+
+    public class Card : Stats
     {
         public string Name { get; set; }
         public int Tier { get; set; }
@@ -12,11 +20,6 @@ namespace SurrealCB.Data.Model
         public Element Element { get; set; }
         public CardPassive Passive { get; set; }
         public List<CardBoost> LevelBoosts { get; set; }
-        public int Hp { get; set; }
-        public int Atk { get; set; }
-        public int Def { get; set; }
-        public int Imm { get; set; } //Immunity
-        public double Spd { get; set; }
         public int Value { get; set; }
         public int BaseExp { get; set; }
         public int RuneSlots { get; set; }
