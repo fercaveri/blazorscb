c787757d52426f9c1e274126fcb5cf84a9bc2022
fercaveri <ferc4@hotmail.com>
2020-03-31 20:55:54 -0300

    Update Map.cs

--- SurrealCB.Data/Model/Map.cs
+++ SurrealCB.Data/Model/Map.cs
@@ -6,6 +6,8 @@ namespace SurrealCB.Data.Model
     {
         public string Name { get; set; }
         public int MinLevel { get; set; }
+        public GameType Type { get; set; }
+//Normal (4 cards) , Endurace ( 4 cards hp refill) , 10 Cards, ??
         public List<EnemyNpc> Enemies { get; set; }
         public List<Reward> CompletionRewards { get; set; }
         //Puede ser GoldReward, ItemReward, ??
